ba92f7e437e9e472afc2e0f5134ebeaeb5a69221
Nikolas Rummel <nikolasrummel6@gmail.com>
2024-05-14 22:13:57 +0200

    added recursive binary search

--- src/main/java/de/dhbw/dsa/BinarySearch.java
+++ src/main/java/de/dhbw/dsa/BinarySearch.java
@@ -31,13 +31,31 @@ public class BinarySearch {
     }
 
 
+    public int recursiveBinarySearch(int[] arr, int key, int i, int j) {
+        System.out.println("Rerusive call with i: " + i + " j: " + j);
+        if (i <= j) {
+            int middle = i + (j - i) / 2;
+
+            if (arr[middle] == key) {
+                return middle;
+            }
+
+            if (arr[middle] < key) {
+                return recursiveBinarySearch(arr, key, middle + 1, j);
+            } else {
+                return recursiveBinarySearch(arr, key, i, middle - 1);
+            }
+        }
+
+        return -1;
+    }
 
 
     public static void main(String[] args) {
         BinarySearch binarySearch = new BinarySearch();
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int key = 4;
-        int result = binarySearch.iterativeBinarySearch(array, key);
+        int result = binarySearch.recursiveBinarySearch(array, key, 0 , array.length - 1);
         if (result == -1) {
             System.out.println("Element not found");
         } else {
